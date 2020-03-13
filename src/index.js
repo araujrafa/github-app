54a96ebb6b1c110051782688d650ce2f124ae692
Rafael Araujo <araujrafa@air-de-rafael.home>
2020-03-12 21:59:24 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+import './config/ReactotronConfig';
+
+const styles = StyleSheet.create({
+});
+
+const App = () => (
+  <View>
+    <Text>Ola</Text>
+  </View>
+);
+
+export default App;
